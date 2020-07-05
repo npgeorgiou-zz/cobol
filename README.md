752d3205d46fc8337090c0bfa8c3c1eae64ce77c
nikos <np@lendme.dk>
2020-07-05 18:44:48 +0200

    stuff

--- README.md
+++ README.md
@@ -5,4 +5,10 @@
 * 01 foo pic a(2) value zero gives error    while  01 foo1 pic a(2) value 0 gives warning.
 * 01 foo pic 9(2) value all 2 gives error   while add all 2 to foo doesn't.
 * 01 foo pic 9(2)v9(2) value all 11 gives error, while 01 foo pic 9(2)v9(2) value all '11'  doesn't. 
-* 01 foo pic 9(2)v9(2) value all "##" fills only integer part of item with "##". 
+*   1 boo.
+      2 boo2 pic 9(2)v9(5) value all '#'.
+      
+        display boo.  *> ######
+        display boo2. *> ##.00000
+
+
--- src/hello.cob
+++ src/hello.cob
@@ -9,16 +9,30 @@ data division.
      88 MALE value 'M'.
      88 FEMALE value 'F'.
      88 BOTH value 'M', 'F'.
+  1 boo.
+    2 boo1 pic a(2) value all 'a'.
+    2 boo2 pic 9(2) value all '8'.
 procedure division.
-  display person"end".
-  set BOTH to TRUE.
-  set BOTH of person to TRUE.
-  display person"end".
+*>  display person"end".
+*>  set BOTH to TRUE.
+*>  set BOTH of person to TRUE.
+*>  display person"end".
+*>
+*>  if MALE
+*>   display person"end"
+*>  end-if.
 
-  if MALE
-   display person"end"
-  end-if.
 
+*>  display boo.
+*>
+*>  initialize boo.
+*>  display boo.
+*>
+*>  move all 't' to boo.
+*>  display boo.
+
+  add zero 1 to boo2.
+  display boo2.
 
 end program hello.
 
