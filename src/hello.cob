c4c13ca8f65a8288a571fe63bdb442b0dae3c346
nikos <np@lendme.dk>
2020-07-17 16:48:43 +0200

    asdsa

--- src/hello.cob
+++ src/hello.cob
@@ -29,7 +29,7 @@ procedure division.
     when is positive display "ok"
     when is numeric display "ok"
     when 1 through 2 display "ok"
-    when 1  display "ok"
+    when 1 display "ok"
   end-evaluate.
 
   evaluate 10 > 0
@@ -40,7 +40,7 @@ procedure division.
     when is positive display "ok"
     when is numeric display "ok"
     when 1 through 2 display "ok"
-    when zero  display "ok"
+    when zero display "ok"
   end-evaluate.
 
   evaluate (10 + 3)
@@ -49,8 +49,8 @@ procedure division.
     when >= 12 display "ok"
     when is numeric display "ok"
     when 1 through 2 display "ok"
-    when "sasa"  display "ok"
-    when any  display "ok"
+    when "sasa" display "ok"
+    when any display "ok"
     *> Errors
     when MALE display "ok"
     when 10 > 2 display "ok"
@@ -58,57 +58,48 @@ procedure division.
   end-evaluate.
 
 
+ *> All these should be OK.
+  evaluate "sda" alphabetic
+    when true
+      display "oasdadsadsak"
+    when other
+      display "other"
+  end-evaluate.
 
+  evaluate foo also "a"
+    when is numeric also is alphabetic
+      display "ok"
+    when not zero also not 3 through 25
+    when not= 1 also not 3 through 25
+    when = 1 also not 3 through 25
+    when equals to 1 also not 3 through 25
+      display "ok"
+    when other
+      display "other"
+  end-evaluate.
 
+  evaluate quote also (1 + +2) ** (6 - 6)/2
+    when   quote also > 0
+      display "ok"
+    when any also any
+      display "any"
+    when other
+      display "other"
+  end-evaluate.
 
+  evaluate true
+    when "r" is alphabetic
+      display "ok"
+  end-evaluate.
 
-
-
-
-
-
-*>  evaluate "sda" alphabetic
-*>    when   true
-*>      display "oasdadsadsak"
-*>    when other
-*>      display "other"
-*>  end-evaluate.
-*>
-*>  evaluate foo also "a"
-*>    when is numeric also is alphabetic
-*>      display "ok"
-*>    when not zero also not 3 through 25
-*>    when not= 1 also not 3 through 25
-*>    when = 1 also not 3 through 25
-*>    when equals to 1 also not 3 through 25
-*>      display "ok"
-*>    when other
-*>      display "other"
-*>  end-evaluate.
-*>
-*>  evaluate quote also (1 + +2) ** (6 - 6)/2
-*>    when   quote also > 0
-*>      display "ok"
-*>    when any also any
-*>      display "any"
-*>    when other
-*>      display "other"
-*>  end-evaluate.
-*>
-*>  evaluate true
-*>    when   "r" is alphabetic
-*>      display "ok"
-*>  end-evaluate.
-*>
-*>  evaluate true also false also MALE also foo alphabetic
-*>    when   true also false also true also false
-*>      display "ok"
-*>    when   true also false also true also false
-*>      display "ok"
-*>    when other
-*>      display "other"
-*>  end-evaluate.
-
+  evaluate true also false also MALE also foo alphabetic
+    when true also false also true also false
+      display "ok"
+    when true also false also true also false
+      display "ok"
+    when other
+      display "other"
+  end-evaluate.
 
 end program hello.
 *> TODO: Change CobolRenamesItemDecl_|CobolGroupItemDecl_|CobolConditionalItemDecl_ to same class with is* methods.
